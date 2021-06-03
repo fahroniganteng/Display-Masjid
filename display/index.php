751efd161e869bdc24037b0efb8e0835cf2c7667
fahroniganteng <62476055+fahroniganteng@users.noreply.github.com>
2021-06-03 17:15:26 +0700

    Update index.php
    tambah icon

--- display/index.php
+++ display/index.php
@@ -49,6 +49,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Display|Masjid</title>
+    <link rel="icon" type="image/png" href="../icon.png"/>
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
